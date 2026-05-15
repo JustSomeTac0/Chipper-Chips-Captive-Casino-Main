504823e6215dd9994ac7153f564deacf6d5df376
JustSomeTac0 <tacoboixd@gmail.com>
2026-05-15 10:59:21 -0400

    Effect Stuff is working

---
+++ Chipper-Chips-Captive-Casino-Main-main/Scenes/effects.gd
@@ -0,0 +1,23 @@
+extends Node2D
+var effectList = []
+var effect
+
+
+#################all effects##############
+var Energized
+
+func _ready() -> void:
+	Energized = $Energized
+
+
+func handleEffects(effect): #just call the with the name of the effect like handleEffects(Energized)
+	var moveAmount = 50
+	if effect in effectList: #this is for when the effect is already active
+		effect.effectTimer.start()
+	else:
+		effectList.push_back(effect) #put effect as the last item
+		moveAmount = moveAmount * effectList.find(effect) #this is for y position
+		effect.global_position.y = moveAmount
+		for i in range(9): #cool popout
+			effect.position.x = effect.position.x - 10
+			await get_tree().create_timer(0.01).timeout
