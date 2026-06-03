b807ae6d263ec688e6be0c684d4602cd0f34a4bf
JustSomeTac0 <tacoboixd@gmail.com>
2026-06-02 22:35:42 -0400

    More stuff not all done tho :p

--- Chipper-Chips-Captive-Casino-Main-main/Scripts/gambling_game.gd
+++ Chipper-Chips-Captive-Casino-Main-main/Scripts/gambling_game.gd
@@ -6,6 +6,7 @@ var LeverArea
 var StartArea
 var PullDownCollision
 var LeverStartCollision
+var Arm
 
 var PullingDown = false
 var InStartArea = false
@@ -13,6 +14,9 @@ var InLeverArea = false
 
 var CollisionHeightUp
 var CollisionHeightDown
+var GapForEachFrame
+
+
 
 
 func _ready() -> void:
@@ -21,11 +25,12 @@ func _ready() -> void:
 	StartArea = $SlotMachine/StartArea
 	PullDownCollision = $SlotMachine/LeverArea/WaysDownCollision
 	LeverStartCollision = $SlotMachine/StartArea/StartCollision
-	
+	Arm = $Arm
 	
 	CollisionHeightDown = SlotMachine.position.y + 42
 	CollisionHeightUp = CollisionHeightDown - 276
-
+	GapForEachFrame = (CollisionHeightDown - CollisionHeightUp) / 5 #<----- last number here is total frame number
+	
 # y = 125 on the top and 401 the bottom
 # 276 diffrance 
 # slot position is  359 so 42 diffrance 
@@ -36,14 +41,35 @@ func _ready() -> void:
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouse:
 		if Input.is_action_pressed("LeftClick"):
+			Arm.play("grab")
 			if InStartArea == true:
 				PullingDown = true
 		elif Input.is_action_just_released("LeftClick"):
 			PullingDown = false
+			Arm.play("wait")
 		
+		var MousesYVaule = get_global_mouse_position().y
+		var MousesXVaule = get_global_mouse_position().x
 		
+		if PullingDown == true:
+			
+			
+			if MousesYVaule >= CollisionHeightUp and MousesYVaule <= (CollisionHeightUp + GapForEachFrame):
+				SlotMachine.frame = 0
+			elif MousesYVaule >= (CollisionHeightUp + GapForEachFrame) and MousesYVaule <= (CollisionHeightUp + (GapForEachFrame * 2)) :
+				SlotMachine.frame = 1
+			elif MousesYVaule >= (CollisionHeightUp + (GapForEachFrame * 2)) and MousesYVaule <= (CollisionHeightUp + (GapForEachFrame * 3)) :
+				SlotMachine.frame = 2
+			elif MousesYVaule >= (CollisionHeightUp + (GapForEachFrame * 3)) and MousesYVaule <= (CollisionHeightUp + (GapForEachFrame * 4)) :
+				SlotMachine.frame = 3
+			elif MousesYVaule >= (CollisionHeightUp + (GapForEachFrame * 4)) and MousesYVaule <= CollisionHeightDown:
+				SlotMachine.frame = 4
+				SlotMachine.play("Spin")
+			else:
+				SlotMachine.frame = 0
 		
-		
+		Arm.position.x = MousesXVaule
+		Arm.position.y = MousesYVaule
 
 
 
@@ -65,3 +91,4 @@ func _on_lever_area_mouse_entered() -> void:
 
 func _on_lever_area_mouse_exited() -> void:
 	InLeverArea = false
+	PullingDown = false
